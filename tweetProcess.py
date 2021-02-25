15f006c040250bd38b04d30c2a131ffc98066822
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-02-25 14:07:26 +0800

    fix 推特改版

--- tweetProcess.py
+++ tweetProcess.py
@@ -69,7 +69,7 @@ class Processor:
         self.modify_tweet()
         # save and clip tweet image
         clip_info = self.driver.execute_script('''
-        return document.querySelector("article .css-1dbjc4n .r-vpgt9t").getBoundingClientRect();''')
+        return document.querySelector("article .css-1dbjc4n.r-1r5su4o").getBoundingClientRect();''')
         self.driver.save_screenshot(
             f'cache_image.png')
         img = Image.open('cache_image.png')
@@ -87,12 +87,12 @@ class Processor:
         # execute js
         print("execute tweet_single js")
         self.driver.execute_script(f'''
-            document.querySelector(".css-1dbjc4n.r-156q2ks").innerHTML += `{template}`''')
+            document.querySelector(".css-1dbjc4n.r-1s2bzr4").innerHTML += `{template}`''')
         img_name = self.save_screenshot()
         return img_name
 
     def process_tweet_retweet(self):
-        selector = '''document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-1gm7m50.r-1ljd8xs.r-13l2t4g.r-1phboty.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(2) > div > section > div > div > div:nth-child(1) > div > div > article > div > div > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div.css-1dbjc4n.r-1bs4hfb.r-1867qdf.r-rs99b7.r-1loqt21.r-dap0kf.r-1ny4l3l.r-1udh08x.r-o7ynqc.r-6416eg > div > div.css-1dbjc4n.r-15d164r.r-vlx1xi")'''
+        selector = '''document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-1gm7m50.r-1ljd8xs.r-13l2t4g.r-1phboty.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(2) > div > section > div > div > div:nth-child(1) > div > div > article > div > div > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div.css-1dbjc4n.r-1bs4hfb.r-1867qdf.r-rs99b7.r-1loqt21.r-adacv.r-1ny4l3l.r-1udh08x.r-o7ynqc.r-6416eg > div > div.css-1dbjc4n.r-6gpygo.r-1fz3rvf > div.css-901oao.r-18jsvk2.r-1tl8opc.r-a023e6.r-16dba41.r-ad9z0x.r-14gqq1x.r-bcqeeo.r-bnwqim.r-qvutc0")'''
         text_ok = self.process_text(self.text["retweet"])
         template = RETWEET_TEMP.replace("{T}", text_ok)
         if "KT_IMG" in template:
