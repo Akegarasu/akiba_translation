2010cff5d251f2ee21ea4a928869aaedbf661116
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-03-06 17:45:30 +0800

    fix template_name not exist err

--- akiba_translation/manager.py
+++ akiba_translation/manager.py
@@ -1,16 +1,19 @@
 import tweetProcess
-from utils import template
+from utils import template as t
 from celery import Celery
 from config import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
 
 celery = Celery("api", broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+TEMPLATE = t.load_template()
 
 
 @celery.task(time_limit=300, soft_time_limit=240, bind=True)
 def execute(self, src: dict):
-    if "template" not in src:
-        src["template"] = template.TEMP.get(src["template_name"]
-                                            , template.TEMP["akiba_temp"])
+    if "template" not in src and "template_name" in src:
+        # try to get template, if template is not exists then return default
+        src["template"] = TEMPLATE.get(src["template_name"], TEMPLATE["default"])
+    else:
+        src["template"] = TEMPLATE["default"]
     p = tweetProcess.Processor(src)
     file_name = p.process_tweet()
     return file_name
