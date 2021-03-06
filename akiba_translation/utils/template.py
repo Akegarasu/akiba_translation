62a1e31a2fbfb2d337af9e794f9d5dc6519f7e50
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-03-06 14:33:40 +0800

    fix template

--- akiba_translation/utils/template.py
+++ akiba_translation/utils/template.py
@@ -1,11 +1,11 @@
-RETWEET_TEMP = '''<div style="margin:2px 2px"><style type="text/css">.link{color:#1DA1F2}</style><img src="{KT_IMG}" height="25"></div><div style="font-size: 18px;font-family: Microsoft YaHei UI, Microsoft YaHei, sans-serif;width: 544px;word-break: break-word;">{T}</div></div>'''
+RETWEET_TEMP = '''<div style="margin:2px 2px"><style type="text/css">.link{color:#1DA1F2}</style><img src="{KT_IMG}" height="25"></div><div style="font-size: 18px;font-family: Microsoft YaHei UI, Microsoft YaHei, sans-serif;width: 544px;word-break: break-word;line-height:1.5; font-weight:400; letter-spacing:0.01em">{T}</div></div>'''
 TEMP: dict = {
     "default": {
-        "html": '''<div style="margin:5px 10px"><style type="text/css">.link{color:#1DA1F2}</style><div style="color: #1DA1F2">翻译自日文</div></div><div style="margin:1px 5px;font-size: 27px;font-family: Microsoft YaHei UI, Microsoft YaHei, sans-serif;width: 544px;word-break: break-word;">{T}</div></div>''',
+        "html": '''<div style="margin:5px 10px"><style type="text/css">.link{color:#1DA1F2}</style><div style="color: #1DA1F2">翻译自日文</div></div><div style="margin:1px 5px;font-size: 27px;font-family: Microsoft YaHei UI, Microsoft YaHei, sans-serif;width: 544px;word-break: break-word;line-height:1.5; font-weight:400; letter-spacing:0.01em">{T}</div></div>''',
         "icon_b64": ""
     },
     "default_with_img": {
-        "html": '''<div style="margin:5px 10px"><style type="text/css">.link{color:#1DA1F2}</style><img src="{KT_IMG}" height="25"></div><div style="margin:1px 5px;font-size: 27px;font-family: Microsoft YaHei UI, Microsoft YaHei, sans-serif;width: 544px;word-break: break-word;">{T}</div></div>''',
+        "html": '''<div style="margin:5px 10px"><style type="text/css">.link{color:#1DA1F2}</style><img src="{KT_IMG}" height="25"></div><div style="margin:1px 5px;font-size: 27px;font-family: Microsoft YaHei UI, Microsoft YaHei, sans-serif;width: 544px;word-break: break-word;line-height:1.5; font-weight:400; letter-spacing:0.01em">{T}</div></div>''',
         "icon_b64": ""
     }
 }
